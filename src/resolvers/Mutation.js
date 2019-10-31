5b08035bad001c150a69eefb9ab15279ae131011
Hesam Bayat <hesam.bayat@gmail.com>
2019-10-31 23:16:47 +0400

    Validate create user args

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -6,9 +6,25 @@ import hashPassword from '../utils/hashPassword'
 const Mutation = {
   async createUser(parent, args, { prisma }, info) {
     const password = await hashPassword(args.data.password)
+
+    if (!args.data.name.trim()) {
+      throw new Error('Name is required')
+    }
+
+    const email = String(args.data.email).trim().toLowerCase()
+    if (!email) {
+      throw new Error('Email is required')
+    }
+
+    const reg = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+    if (!reg.test(email)) {
+      throw new Error('Enter a valid email')
+    }
+
     const user = await prisma.mutation.createUser({
       data: {
         ...args.data,
+        email,
         password
       }
     })
